04273ffcea2a8bcd608e119534ac16fe65a0893e
stron <yaneliyahu83@gmail.com>
2024-04-27 23:44:05 +0300

    chore(home-page): remove redundant hook import

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,10 +1,8 @@
-import { Header } from "@components/header";
 import "./home-page.scss";
-import { useGetIpInfo } from "@hooks/use-get-ip-info";
+import { Header } from "@components/header";
 import { SearchContextProvider } from "src/store/providers/search-context-provider";
 
 export function HomePage() {
-
   return (
     <SearchContextProvider>
       <main>

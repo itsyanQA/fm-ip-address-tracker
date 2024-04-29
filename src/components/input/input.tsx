b94ddc9364f4b8036babe91d38d1e169a6730e93
stron <yaneliyahu83@gmail.com>
2024-04-29 18:14:07 +0300

    feat(input): set isError to false if searchValue is truthy

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,6 +1,6 @@
 import Arrow from "@assets/icon-arrow.svg?react";
 import "./input.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchQueryContext } from "src/store/context/use-search-context";
 
 export function Input() {
@@ -9,22 +9,27 @@ export function Input() {
   const { setSearchedAddress } = useSearchQueryContext();
 
   const buttonHandler = () => {
-    if (!searchValue) setIsError(true);
-
     if (searchValue) {
       setIsError(false);
       setSearchedAddress(searchValue);
+    } else {
+      setIsError(true);
     }
   };
 
+  useEffect(() => {
+    if (searchValue) setIsError(false);
+  }, [searchValue]);
+
   return (
-    <div className={`input-container ${isError ? "input-container--is-error" : ""}`}>
+    <div className={`input-container ${isError && !searchValue ? "input-container--is-error" : ""}`}>
       <input
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         onKeyDown={(e) => (e.key === "Enter" ? buttonHandler() : null)}
         type="text"
         placeholder="Search for any IP address or domain"
+        maxLength={100}
       />
       <button onClick={buttonHandler}>{<Arrow />}</button>
     </div>

fa3e73167a22f097ab4d1d66755210f645e8bcd0
stron <yaneliyahu83@gmail.com>
2024-04-29 15:30:31 +0300

    style(input): add an indiciator when searching with no value

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -5,16 +5,24 @@ import { useSearchQueryContext } from "src/store/context/use-search-context";
 
 export function Input() {
   const [searchValue, setSearchValue] = useState<string>("");
+  const [isError, setIsError] = useState(false);
   const { setSearchedAddress } = useSearchQueryContext();
+
   const buttonHandler = () => {
-    if (searchValue) setSearchedAddress(searchValue);
+    if (!searchValue) setIsError(true);
+
+    if (searchValue) {
+      setIsError(false);
+      setSearchedAddress(searchValue);
+    }
   };
 
   return (
-    <div className="input-container">
+    <div className={`input-container ${isError ? "input-container--is-error" : ""}`}>
       <input
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
+        onKeyDown={(e) => (e.key === "Enter" ? buttonHandler() : null)}
         type="text"
         placeholder="Search for any IP address or domain"
       />
